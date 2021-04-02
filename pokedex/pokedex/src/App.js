0bc777575626760371b712d0c2389f7f33a9dc20
Eperuza <jeremy.riv@outlook.com>
2021-04-02 11:50:19 -0400

    Completed a couple items

--- pokedex/pokedex/src/App.js
+++ pokedex/pokedex/src/App.js
@@ -7,11 +7,11 @@ import getPokemonId from './utils/GetPokemonId.js'
 
 //[ DONE ] Home page (list all pokemon first 150 w/ picture)
 //[ DONE] Click on Pokemon to view additional info path is /pokemon/bulbasaur/ goes to abilities
-//[ ] view info - abilities button - presented with a panel that defaults with their abilities
-//[ ] view info - environment button - change panel to abilities.
+//[ DONE ] view info - abilities button - presented with a panel that defaults with their abilities
+//[ DONE] view info - environment button - change panel to abilities.
 //[ ] About page - read info about the team that developed app
 //[ ] On home page Dropdown box to filter pokemon by their types
-//[ ] example “/pokemon/bulbasaur/environment” path that shows the environment panel for bulbasaour
+//[DONE ] example “/pokemon/bulbasaur/environment” path that shows the environment panel for bulbasaour
 // pokemon list - "https://pokeapi.co/api/v2/pokemon/?limit=20"
 // abilities - "https://pokeapi.co/api/v2/pokemon/1/"
 // environment (location area encounters) - "https://pokeapi.co/api/v2/pokemon/1/encounters"
